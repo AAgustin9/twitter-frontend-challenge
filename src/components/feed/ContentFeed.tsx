9cc14a860b1848d6de256ba6ec459e4f280359ae
agussoul <aalma@mail.austral.edu.ar>
2025-06-29 17:00:52 -0300

    styled refresh button

--- src/components/feed/ContentFeed.tsx
+++ src/components/feed/ContentFeed.tsx
@@ -2,6 +2,38 @@ import React, { useEffect } from "react";
 import Feed from "./Feed";
 import { useInfiniteFeed } from "../../hooks/queries/useInfiniteFeed";
 import { useAppSelector } from "../../redux/hooks";
+import styled from "styled-components";
+import { Icon, IconType } from "../icon/Icon";
+
+const RefreshContainer = styled.div`
+  width: 100%;
+  text-align: left;
+  padding: 16px;
+`;
+
+const RefreshButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  border-radius: 50%;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  color: ${props => props.theme.colors.main};
+  transition: background 0.2s, transform 0.1s;
+
+  &:hover {
+    background: ${props => props.theme.hover.default};
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
 
 const ContentFeed = () => {
   const query = useAppSelector((s) => s.user.query)
@@ -38,13 +70,18 @@ const ContentFeed = () => {
     }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
 
   if (isError) return <div>Oops, could not load feed.</div>
+  
   return (
     <>
-      <div style={{ width: "100%", textAlign: "left", padding: "16px 16px" }}>
-        <button onClick={() => refetch()} disabled={isLoading}>
-          Refresh
-        </button>
-      </div>
+      <RefreshContainer>
+        <RefreshButton
+          onClick={() => refetch()}
+          disabled={isLoading}
+          title="Refresh feed"
+        >
+          {Icon({ width: "20px", height: "20px" })[IconType.RETWEET]}
+        </RefreshButton>
+      </RefreshContainer>
       <Feed posts={allPosts} loading={isLoading || isFetchingNextPage} />
     </>
   );
